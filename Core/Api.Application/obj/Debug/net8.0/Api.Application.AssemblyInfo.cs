621c169e263bbb12ff47dcaeec4f0abe5d8376ec
Hasan Ali İlter <hasanilter003@gmail.com>
2024-07-12 12:59:35 +0300

    Parsing of Production and Development environments and appsettings configurations

--- Core/Api.Application/obj/Debug/net8.0/Api.Application.AssemblyInfo.cs
+++ Core/Api.Application/obj/Debug/net8.0/Api.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+782f75dfe236059263e65aa5527f7bba1aa5a508")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
