9b663616ee37653ea75eb6908bf24b83c02c5ee4
Hasan Ali İlter <hasanilter003@gmail.com>
2024-07-16 17:33:16 +0300

    fix

--- Infrastructure/Api.Persistence/obj/Debug/net8.0/Api.Persistence.AssemblyInfo.cs
+++ Infrastructure/Api.Persistence/obj/Debug/net8.0/Api.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac19d0e00a702e6015fb25c3f455ba1bc0551f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
