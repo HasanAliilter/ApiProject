9162d896a591f8c1e18aa82926fc743b492bf1ee
Hasan Ali İlter <hasanilter003@gmail.com>
2024-07-24 22:09:35 +0300

    conflict fix

--- Api.Mapper/obj/Debug/net8.0/Api.Mapper.AssemblyInfo.cs
+++ Api.Mapper/obj/Debug/net8.0/Api.Mapper.AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+210ae14f4e8251933ac322cf7cf52b62e463d225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api.Mapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api.Mapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Presentation/ProjectApi.Api/obj/Debug/net8.0/ProjectApi.Api.AssemblyInfo.cs
+++ Presentation/ProjectApi.Api/obj/Debug/net8.0/ProjectApi.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectApi.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+210ae14f4e8251933ac322cf7cf52b62e463d225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectApi.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectApi.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
