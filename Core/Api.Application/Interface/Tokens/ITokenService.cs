6428a0ab528278afe74d01da762462b42437487a
Hasan Ali İlter <hasanilter003@gmail.com>
2024-08-02 14:57:03 +0300

    feat : Token Service and Create Token Method were made

---
+++ Core/Api.Application/Interface/Tokens/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using Api.Domain.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Api.Application.Interface.Tokens
+{
+    public interface ITokenService
+    {
+        Task<JwtSecurityToken> CreateToken(User user, IList<string> roles);
+        string GenerateRefreshToken();
+        ClaimsPrincipal? GetPrincipalFromExpiredToken();
+    }
+}
---
+++ Infrastructure/Api.Infrastructure/Registration.cs
@@ -0,0 +1,14 @@
+﻿using Api.Infrastructure.Tokens;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Infrastructure
+{
+    public static class Registration
+    {
+        public static void AddInfratsructure(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.Configure<TokenSettings>(configuration.GetSection("JWT"));
+        }
+    }
+}
